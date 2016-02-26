971d2c5d61f17774a6d8d137ca3ad87e2883048f
Dmitry Ivanov <ivadmi5@gmail.com>
2016-02-26 16:29:45 +0300

    dummy node works

--- init.c
+++ init.c
@@ -1,5 +1,6 @@
 #include "postgres.h"
 #include "optimizer/paths.h"
+#include "optimizer/pathnode.h"
 #include "optimizer/restrictinfo.h"
 #include "utils/builtins.h"
 #include "utils/elog.h"
@@ -9,6 +10,9 @@
 #include "catalog/pg_operator.h"
 #include "commands/explain.h"
 #include "funcapi.h"
+
+#include "access/htup_details.h"
+#include "point.h"
 
 extern void _PG_init(void);
 
@@ -26,73 +30,50 @@ typedef struct
 	List		   *joinrestrictinfo;
 } CrossmatchJoinPath;
 
+typedef struct
+{
+	CustomScanState css;
+
+	HeapTupleData	scan_tuple;		/* buffer to fetch tuple */
+	List		   *dev_tlist;		/* tlist to be returned from the device */
+	List		   *dev_quals;		/* quals to be run on the device */
+} CrossmatchScanState;
+
 static CustomPathMethods	crossmatch_path_methods;
 static CustomScanMethods	crossmatch_plan_methods;
 static CustomExecMethods	crossmatch_exec_methods;
 
-void join_pathlist_hook (PlannerInfo *root,
-						 RelOptInfo *joinrel,
-						 RelOptInfo *outerrel,
-						 RelOptInfo *innerrel,
-						 JoinType jointype,
-						 JoinPathExtraData *extra)
+
+static Path *
+crossmatch_find_cheapest_path(PlannerInfo *root,
+							  RelOptInfo *joinrel,
+							  RelOptInfo *inputrel)
 {
-	ListCell   *restr;
-	text	   *dist_func_name = cstring_to_text("dist(spoint,spoint)");
-	Oid			dist_func;
+	Path	   *input_path = inputrel->cheapest_total_path;
+	Relids		other_relids;
+	ListCell   *lc;
 
-	dist_func = DatumGetObjectId(DirectFunctionCall1(to_regprocedure,
-													 PointerGetDatum(dist_func_name)));
-
-	if (dist_func == InvalidOid)
-		elog(ERROR, "function dist not found!");
-
-	if (set_join_pathlist_next)
-		set_join_pathlist_next(root,
-							   joinrel,
-							   outerrel,
-							   innerrel,
-							   jointype,
-							   extra);
-
-	foreach(restr, extra->restrictlist)
+	other_relids = bms_difference(joinrel->relids, inputrel->relids);
+	if (bms_overlap(PATH_REQ_OUTER(input_path), other_relids))
 	{
-		RestrictInfo *restrInfo = (RestrictInfo *) lfirst(restr);
-
-		if (IsA(restrInfo->clause, OpExpr))
+		input_path = NULL;
+		foreach (lc, inputrel->pathlist)
 		{
-			OpExpr	   *opExpr = (OpExpr *) restrInfo->clause;
-			int			nargs = list_length(opExpr->args);
-			Node	   *arg1;
-			Node	   *arg2;
+			Path   *curr_path = lfirst(lc);
 
-			if (nargs != 2)
+			if (bms_overlap(PATH_REQ_OUTER(curr_path), other_relids))
 				continue;
-
-			arg1 = linitial(opExpr->args);
-			arg2 = lsecond(opExpr->args);
-
-			if (opExpr->opno == Float8LessOperator &&
-				IsA(arg1, FuncExpr) &&
-				((FuncExpr *) arg1)->funcid == dist_func &&
-				IsA(arg2, Const))
-			{
-				elog(LOG, "found <");
-			}
-			else if (opExpr->opno == get_commutator(Float8LessOperator) &&
-					 IsA(arg1, Const) &&
-					 IsA(arg2, FuncExpr) &&
-					 ((FuncExpr *) arg2)->funcid == dist_func)
-			{
-				elog(LOG, "found >");
-			}
+			if (input_path == NULL ||
+				input_path->total_cost > curr_path->total_cost)
+				input_path = curr_path;
 		}
 	}
+	bms_free(other_relids);
 
-	pprint(root->parse->rtable);
+	return input_path;
 }
 
-static CrossmatchJoinPath *
+static void
 create_crossmatch_path(PlannerInfo *root,
 					   RelOptInfo *joinrel,
 					   Path *outer_path,
@@ -117,11 +98,116 @@ create_crossmatch_path(PlannerInfo *root,
 	result->inner_path = inner_path;
 	result->joinrestrictinfo = restrict_clauses;
 
-	/* DEBUG costs */
+	/* TODO: real costs */
 	result->cpath.path.startup_cost = 1;
 	result->cpath.path.total_cost = 1;
 
-	return result;
+	add_path(joinrel, &result->cpath.path);
+}
+
+static void
+join_pathlist_hook(PlannerInfo *root,
+				   RelOptInfo *joinrel,
+				   RelOptInfo *outerrel,
+				   RelOptInfo *innerrel,
+				   JoinType jointype,
+				   JoinPathExtraData *extra)
+{
+	ListCell   *restr;
+	text	   *dist_func_name = cstring_to_text("dist(spoint,spoint)");
+	Oid			dist_func;
+	List	   *restrict_clauses = extra->restrictlist;
+
+	dist_func = DatumGetObjectId(DirectFunctionCall1(to_regprocedure,
+													 PointerGetDatum(dist_func_name)));
+
+	if (dist_func == InvalidOid)
+		elog(ERROR, "function dist not found!");
+
+	if (set_join_pathlist_next)
+		set_join_pathlist_next(root,
+							   joinrel,
+							   outerrel,
+							   innerrel,
+							   jointype,
+							   extra);
+
+	foreach(restr, extra->restrictlist)
+	{
+		RestrictInfo *restrInfo = (RestrictInfo *) lfirst(restr);
+
+		if (outerrel->reloptkind == RELOPT_BASEREL &&
+			innerrel->reloptkind == RELOPT_BASEREL &&
+			bms_is_member(outerrel->relid, restrInfo->clause_relids) &&
+			bms_is_member(innerrel->relid, restrInfo->clause_relids))
+		{
+			/* This is our case */
+		}
+		else continue;
+
+		if (IsA(restrInfo->clause, OpExpr))
+		{
+			OpExpr	   *opExpr = (OpExpr *) restrInfo->clause;
+			int			nargs = list_length(opExpr->args);
+			Node	   *arg1;
+			Node	   *arg2;
+
+			if (nargs != 2)
+				continue;
+
+			arg1 = linitial(opExpr->args);
+			arg2 = lsecond(opExpr->args);
+
+			if (opExpr->opno == Float8LessOperator &&
+				IsA(arg1, FuncExpr) &&
+				((FuncExpr *) arg1)->funcid == dist_func &&
+				IsA(arg2, Const))
+			{
+				Path *outer_path = crossmatch_find_cheapest_path(root, joinrel, outerrel);
+				Path *inner_path = crossmatch_find_cheapest_path(root, joinrel, innerrel);
+
+				Relids required_outer = calc_nestloop_required_outer(outer_path, inner_path);
+
+				ParamPathInfo *param_info = get_joinrel_parampathinfo(root,
+																	  joinrel,
+																	  outer_path,
+																	  inner_path,
+																	  extra->sjinfo,
+																	  required_outer,
+																	  &restrict_clauses);
+
+				/* DEBUG */
+				create_crossmatch_path(root, joinrel, outer_path, inner_path,
+									   param_info, restrict_clauses, required_outer);
+
+				break;
+			}
+			else if (opExpr->opno == get_commutator(Float8LessOperator) &&
+					 IsA(arg1, Const) &&
+					 IsA(arg2, FuncExpr) &&
+					 ((FuncExpr *) arg2)->funcid == dist_func)
+			{
+				Path *outer_path = crossmatch_find_cheapest_path(root, joinrel, outerrel);
+				Path *inner_path = crossmatch_find_cheapest_path(root, joinrel, innerrel);
+
+				Relids required_outer = calc_nestloop_required_outer(outer_path, inner_path);
+
+				ParamPathInfo *param_info = get_joinrel_parampathinfo(root,
+																	  joinrel,
+																	  outer_path,
+																	  inner_path,
+																	  extra->sjinfo,
+																	  required_outer,
+																	  &restrict_clauses);
+
+				/* DEBUG */
+				create_crossmatch_path(root, joinrel, outer_path, inner_path,
+									   param_info, restrict_clauses, required_outer);
+			}
+		}
+	}
+
+	pprint(root->parse->rtable);
 }
 
 static Plan *
@@ -153,30 +239,67 @@ create_crossmatch_plan(PlannerInfo *root,
 	cscan = makeNode(CustomScan);
 	cscan->scan.plan.targetlist = tlist;
 	cscan->scan.plan.qual = NIL;
+	cscan->scan.scanrelid = 0;
+
+	cscan->custom_scan_tlist = tlist;
+
+	elog(LOG, "tlist:");
+	pprint(tlist);
 
 	cscan->flags = best_path->flags;
 	cscan->methods = &crossmatch_plan_methods;
 	cscan->custom_plans = list_copy_tail(custom_plans, 1);
 
-
+	return &cscan->scan.plan;
 }
 
 static Node *
 crossmatch_create_scan_state(CustomScan *node)
 {
-	return NULL;
+	CrossmatchScanState *scan_state = palloc0(sizeof(CrossmatchScanState));
+
+	NodeSetTag(scan_state, T_CustomScanState);
+	scan_state->css.flags = node->flags;
+	if (node->methods == &crossmatch_plan_methods)
+		scan_state->css.methods = &crossmatch_exec_methods;
+	else
+		elog(ERROR, "Bug? unexpected CustomPlanMethods");
+
+	return (Node *) scan_state;
 }
+
+static int i = 0;
 
 static void
 crossmatch_begin(CustomScanState *node, EState *estate, int eflags)
 {
-
+	i = 0;
 }
 
 static TupleTableSlot *
 crossmatch_exec(CustomScanState *node)
 {
-	return NULL;
+	TupleTableSlot *slot = node->ss.ss_ScanTupleSlot;
+	TupleDesc	tupdesc = node->ss.ss_ScanTupleSlot->tts_tupleDescriptor;
+	HeapTuple	htup;
+
+	/* TODO: fill with real data from joined tables */
+	Datum values[2] = { DirectFunctionCall1(spherepoint_in, CStringGetDatum("(0d, 0d)")),
+						DirectFunctionCall1(spherepoint_in, CStringGetDatum("(0d, 0d)")) };
+	bool nulls[2] = {0, 0};
+
+	htup = heap_form_tuple(tupdesc, values, nulls);
+
+	elog(LOG, "natts: %d", tupdesc->natts);
+
+	i++;
+
+	if (i > 10)
+		ExecClearTuple(slot);
+	else
+		ExecStoreTuple(htup, slot, InvalidBuffer, false);
+
+	return slot;
 }
 
 static void
@@ -206,17 +329,17 @@ _PG_init(void)
 	set_join_pathlist_hook = join_pathlist_hook;
 
 	crossmatch_path_methods.CustomName				= "CrossmatchJoin";
-	crossmatch_path_methods.PlanCustomPath			= &create_crossmatch_plan;
+	crossmatch_path_methods.PlanCustomPath			= create_crossmatch_plan;
 
 	crossmatch_plan_methods.CustomName 				= "CrossmatchJoin";
-	crossmatch_plan_methods.CreateCustomScanState	= &crossmatch_create_scan_state;
+	crossmatch_plan_methods.CreateCustomScanState	= crossmatch_create_scan_state;
 
 	crossmatch_exec_methods.CustomName				= "CrossmatchJoin";
-	crossmatch_exec_methods.BeginCustomScan			= &crossmatch_begin;
-	crossmatch_exec_methods.ExecCustomScan			= &crossmatch_exec;
-	crossmatch_exec_methods.EndCustomScan			= &crossmatch_end;
-	crossmatch_exec_methods.ReScanCustomScan		= &crossmatch_rescan;
+	crossmatch_exec_methods.BeginCustomScan			= crossmatch_begin;
+	crossmatch_exec_methods.ExecCustomScan			= crossmatch_exec;
+	crossmatch_exec_methods.EndCustomScan			= crossmatch_end;
+	crossmatch_exec_methods.ReScanCustomScan		= crossmatch_rescan;
 	crossmatch_exec_methods.MarkPosCustomScan		= NULL;
 	crossmatch_exec_methods.RestrPosCustomScan		= NULL;
-	crossmatch_exec_methods.ExplainCustomScan		= &crossmatch_explain;
+	crossmatch_exec_methods.ExplainCustomScan		= crossmatch_explain;
 }
